ee3ce27017eb9eb9b34af513caa4980f8de64db0
Shimon Asulin <asulin.shimon699@gmail.com>
2022-08-18 00:26:21 +0300

    second meeting about the code

---
+++ src/pages/EmployeesSetup.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const EmployeesSetup = () => {
+  return (
+    <div>EmployeesSetup</div>
+  )
+}
---
+++ src/routes.jsx
@@ -0,0 +1,10 @@
+import Home from "./pages/Home";
+
+let routes = [
+  {
+    index: true,
+    element: <Home />,
+  },
+];
+
+export default routes;
