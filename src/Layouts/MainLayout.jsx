c368bf3049a032390b8289964e5db86785f7f178
Refaelbenzvi24 <refaelbenzvi24@gmail.com>
2022-08-30 11:51:22 +0300

    feat(sideBar): implemented the navigation part of the sidebar - created a navigation component which can take a list of options and a component to render for each one of them - added auto import for local icons - created routes for navigation - created main layout

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,85 @@
+import {Outlet} from "react-router";
+import {
+    AppBar,
+    Avatar,
+    Divider,
+    Icon, Main,
+    Navigation,
+    NavigationLink,
+    Row,
+    SideBar,
+    Typography
+} from "../components/UI/index.jsx";
+import Logo from "../components/Logo.jsx";
+import avatar from "../assets/avatar1.png";
+
+
+const MainLayout = () => {
+
+    return (
+        <div className="h-full">
+            <AppBar className="justify-between">
+                <Row>
+                    <Logo/>
+                    <Divider color={'#A0A0A0'} opacity={0.8}/>
+
+
+                    <Row className="pl-40">
+                        <Icon color="#E8E8E8" size={18}>
+                            <IconMdiBellOutline/>
+                        </Icon>
+                    </Row>
+                </Row>
+
+                <Row
+                    className="pr-36 align-center">
+                    <Icon className="pr-34" size={20} color="#E8E8E8">
+                        <IconIcOutlineSettings/>
+                    </Icon>
+
+                    <Avatar
+                        src={avatar}
+                        borderColor={'#E8E8E8'}
+                        size={38}/>
+
+                    <Typography
+                        className="font-medium pl-10 pr-8"
+                        color="white"
+                        variant="h6">
+                        Francesca
+                    </Typography>
+
+                    <Icon
+                        color="#E8E8E8"
+                        size={12}>
+                        <IconIonChevronDown/>
+                    </Icon>
+                </Row>
+            </AppBar>
+
+            <SideBar>
+                <Navigation
+                    navigationOptions={[
+                        {icon: <IconLocalHomeOutlined/>, activeIcon: <IconLocalHomeFilled/>, to: '/'},
+                        {icon: <IconLocalUserOutlined/>, activeIcon: <IconLocalUserFilled/>, to: '/employees'},
+                        {icon: <IconLocalChatOutlined/>, activeIcon: <IconLocalChatFilled/>, to: '/chat'},
+                    ]}
+                    navigationItem={(activeIndex, setActiveIndex, {icon, activeIcon, to}, index) => (
+                        <NavigationLink
+                            to={to}
+                            key={index}
+                            isActive={activeIndex === index}
+                            onClick={() => setActiveIndex(index)}
+                            icon={icon}
+                            activeIcon={activeIcon}/>
+                    )}/>
+            </SideBar>
+
+            <Main>
+                <Outlet/>
+            </Main>
+        </div>
+    )
+}
+
+export default MainLayout
