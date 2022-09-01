d454c82fdd393638f9d215be9a02c781f306ca47
BrachaS20 <brachasabach@gmail.com>
2022-09-01 13:08:51 +0300

    feat(scheduler): added shifts functionality
    -added functionality of selecting shift template and adding it to the Schedule and updating the data
    -added + icon for insert a new template

---
+++ src/modules/Shift.js
@@ -0,0 +1,9 @@
+export default class Shift {
+    constructor(data) {
+        this.position = data.position
+        this.shift = data.shift
+        this.time = data.time
+        this.status = data.status
+    }
+}
+
